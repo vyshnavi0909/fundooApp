52fe0213c0c38b6768dfa8a537a69fe0eb39adc8
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-06-23 23:53:13 +0530

    Added sign in page and linked it with create new account page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import NewAccount from './components/NewAccount'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignIn from './components/SignIn'
 
 function App() {
   return (
-    <div className="App">
-      <NewAccount/>
+    <Router>
+      <div className="App">
+      <Route exact path="/" component={NewAccount}></Route>
+      <Route exact path="/sign-in" component={SignIn}></Route>
     </div>
+    </Router>
+    
+    
   );
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+
+
+export class SignIn extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+  render() {
+    return (
+      <div id="sign-in-link">
+        <form className="sign-in-form">
+          <div className="fundoo-logo">
+            <svg className="fundoo-logo" height="20" width="100">
+              <text x="6" y="19" fill="blue">
+                F
+              </text>
+              <text x="17" y="19" fill="red">
+                u
+              </text>
+              <text x="32" y="19" fill="orange">
+                n
+              </text>
+              <text x="47" y="19" fill="blue">
+                d
+              </text>
+              <text x="63" y="19" fill="green">
+                o
+              </text>
+              <text x="79" y="19" fill="red">
+                o
+              </text>
+            </svg>
+            <h3 className="sign-in-head">Sign in</h3>
+            <p className="sign-in-tag">Use your Fundoo Account</p>
+          </div>
+          <TextField
+            className="email-field"
+            id="mail-phone"
+            label="Email or phone"
+            variant="outlined"
+          />
+          <div className="middle-box">
+            <a className="forgot-link" href="#forgot-email">Forgot email?</a>
+            <p className="tag">Not your computer? Use Guest mode to sign in privately.</p>
+            <a className="learn-more-link" href="https://support.google.com/chrome/answer/6130773?hl=en">
+              Learn more
+            </a>
+          </div>
+          <div className="bottom-div">
+            <Link className="create-acc" to="/">Create account</Link>
+            <Button variant="contained" color="primary">
+              Next
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
